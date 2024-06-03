d864f149c97a233d47d43f7b3e6feae541c61e13
keyur9874 <73292698+keyur9874@users.noreply.github.com>
2024-06-03 21:52:12 +0530

    Added excel to csv convertor

--- ExcelToCSVConvertor/ConvertorApp/ExcelToCSV.cs
+++ ExcelToCSVConvertor/ConvertorApp/ExcelToCSV.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using ConvertorApp.Helper;
@@ -15,16 +16,40 @@ namespace ConvertorApp
         public ExcelToCSV(ILogger<ExcelToCSV> logger)
         {
             _logger = logger;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
         }
 
         [Function(nameof(ExcelToCSV))]
-        public async Task Run(
-            [BlobTrigger("excel-container/{name}", Connection = "ExeclCSVFileStorage")] byte[] inputBlob,
+        [BlobOutput("csv-container/{name}.csv", Connection = "ExeclCSVFileStorage")]
+        public async Task<string> Run(
+            [BlobTrigger("excel-container/{name}.{type}", Connection = "ExeclCSVFileStorage")] byte[] inputBlob,
             string name,
+            string type,
             FunctionContext context)
         {
             var logger = context.GetLogger("ExcelToCsvFunction");
             logger.LogInformation($"C# Blob trigger function Processed blob\n Name:{name}");
+            using var memoryStream = new MemoryStream(inputBlob);
+            using var reader = ExcelReaderFactory.CreateReader(memoryStream);
+            var result = reader.AsDataSet();
+
+            var csvContent = new StringBuilder();
+            foreach (System.Data.DataTable table in result.Tables)
+            {
+                foreach (System.Data.DataRow row in table.Rows)
+                {
+                    for (var i = 0; i < table.Columns.Count; i++)
+                    {
+                        csvContent.Append(row[i]);
+                        if (i < table.Columns.Count - 1)
+                            csvContent.Append(",");
+                    }
+                    csvContent.AppendLine();
+                }
+            }
+
+            return csvContent.ToString();
         }
     }
 }
